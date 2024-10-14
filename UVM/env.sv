e1690ae26fd69b5649ca967169e28bb04e26fb41
KhaledSarhan01 <khaledsarhaan@gmail.com>
2024-10-14 21:42:09 +0300

    Environment is Created

---
+++ UVM/env.sv
@@ -0,0 +1,36 @@
+package env_pkg;   
+    //import cvg_pkg::*;
+    //import agent_pkg::*;
+    //import scoreboard_pkg::*;
+
+    import uvm_pkg::*;
+    `include "uvm_macros.svh"
+    
+    class FIFO_env extends uvm_env;
+        `uvm_component_utils(FIFO_env)
+        /*
+        FIFO_Coverage FIFO_cover;
+        FIFO_Agent agent;
+        FIFO_Scoreboard scoreboard;
+        */
+        function new(string name = "FIFO_env" , uvm_component parent = null);
+            super.new(name,parent);
+        endfunction
+       /* 
+        function void build_phase(uvm_phase phase);
+            super.build_phase(phase);
+            FIFO_cover  = FIFO_Coverage    ::type_id::create("FIFO_cover",this);
+            agent       = FIFO_Agent       ::type_id::create("agent",this);
+            scoreboard  = FIFO_Scoreboard  ::type_id::create("scoreboard",this);
+        endfunction
+
+        function void connect_phase(uvm_phase phase);
+            super.connect_phase(phase);
+            agent.agent_aport.connect(FIFO_cover.cvg_aport);
+            agent.agent_aport.connect(scoreboard.sb_aport);
+        endfunction
+        */
+    endclass    
+    
+endpackage
+
--- UVM/test.sv
+++ UVM/test.sv
@@ -1,55 +1,54 @@
 package test_pkg;
-    //import env_pkg::*;
-    //import config_pkg::*;
+    import env_pkg::*;
+    import config_pkg::*;
     //import test_sequence_pkg::*;
     import uvm_pkg ::*;
     `include "uvm_macros.svh"
 
     class FIFO_test extends uvm_test;
         `uvm_component_utils(FIFO_test)
-    /*
-        alsu_env env;
-        alsu_config_obj alsu_config_obj_test;
-        reset_sequence rst_seq; 
+    
+        FIFO_env env;
+        FIFO_config_obj FIFO_config_obj_test;
+    /*  reset_sequence rst_seq; 
         sequence_0 seq_0;
         sequence_1 seq_1;
     */
         function new(string name = "FIFO_test",uvm_component parent = null);
             super.new(name,parent);
         endfunction
-    /*
+    
         function void build_phase(uvm_phase phase);
             super.build_phase(phase);
             // Building the Environment
-            env = alsu_env :: type_id :: create("env",this);
-            rst_seq = reset_sequence :: type_id :: create("rst_seq");
-            seq_0   = sequence_0     :: type_id :: create("seq_0");
-            seq_1   = sequence_1     :: type_id :: create("seq_1");
+            env = FIFO_env :: type_id :: create("env",this);
+            //rst_seq = reset_sequence :: type_id :: create("rst_seq");
+            //seq_0   = sequence_0     :: type_id :: create("seq_0");
+            //seq_1   = sequence_1     :: type_id :: create("seq_1");
 
             // Passing the Configuraitons
-            alsu_config_obj_test = alsu_config_obj::type_id::create("alsu_config_obj_test");
-            if(!(uvm_config_db #(virtual alsu_if)::get(this,"","top_alsu_if",alsu_config_obj_test.alsu_config_vif)))
+            FIFO_config_obj_test = FIFO_config_obj::type_id::create("FIFO_config_obj_test");
+            if(!(uvm_config_db #(virtual FIFO_if)::get(this,"","top_FIFO_if",FIFO_config_obj_test.FIFO_config_vif)))
             `uvm_fatal("Test","Unable to get the virtual handle");
-            uvm_config_db #(alsu_config_obj)::set(this,"*","test_alsu_obj",alsu_config_obj_test);
+            uvm_config_db #(FIFO_config_obj)::set(this,"*","test_FIFO_obj",FIFO_config_obj_test);
         endfunction
         
         task run_phase(uvm_phase phase);
             super.run_phase(phase);
             phase.raise_objection(this);
                 `uvm_info("TEST RUN","Reset Assert",UVM_LOW);
-                rst_seq.start(env.agent.sequencer);
+                //rst_seq.start(env.agent.sequencer);
                 `uvm_info("TEST RUN","Reset Deassert",UVM_LOW);
 
                 `uvm_info("TEST RUN","Test 1 Sequence begin",UVM_LOW);
-                seq_0.start(env.agent.sequencer);
+                //seq_0.start(env.agent.sequencer);
                 `uvm_info("TEST RUN","Test 1 Sequence end",UVM_LOW);
 
                 `uvm_info("TEST RUN","Test 2 Sequence begin",UVM_LOW);
-                seq_1.start(env.agent.sequencer);
+                //seq_1.start(env.agent.sequencer);
                 `uvm_info("TEST RUN","Test 2 Sequence end",UVM_LOW);
             phase.drop_objection(this);
         endtask 
-    */    
     endclass
 endpackage
 
